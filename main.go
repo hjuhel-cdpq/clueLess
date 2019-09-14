a9e3ed2e47ab585ac20ec77939569ff7ff80bfc7
hjuhel <hugo.juhel@gmail.com>
2019-09-14 17:38:22 -0400

    Updating dependencies

--- main.go
+++ main.go
@@ -1,24 +1,68 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/hjuhel-cdpq/jloom/encoder"
+	"github.com/tenfyzhong/cityhash"
+	"fmt"
 )
 
-func main() {
-	doc := encoder.Document{"c'est un test je suis , pas mal inquier", "M'en parles pas ! Je ne cesse de me demandé comment va se passer le splut unicode"}
-	tokenized := doc.Tokenize()
+// Build a hash function from a seed
+func BuildHash64Function(seed uint64) encoder.Hash64Function {
 
-	// Transform the document to rune
-	byteList := make([][]byte, len(tokenized.Body))
-	for i, item := range tokenized.Body {
-		r := []byte(item)
-		byteList[i] = r
+	curryHash := func(s []byte) uint64 {
+		hash := cityhash.CityHash64WithSeed(s, seed)
+		return hash
 	}
-	seed := uint64(42)
-	h := encoder.BuildHash64Function(seed)
-	hash := encoder.Minhash(byteList, h, 2)
-	fmt.Println(hash)
 
+	return curryHash
+}
+
+func add(t encoder.CountMinSketchTable, s string, h1, h2 encoder.Hash64Function) (uint8, *encoder.MinWise) {
+
+	// Create a new MinWise Struct
+	mw := encoder.NewMinWise(h1, h2, 100)
+
+	// Append the tokenized string to the mw
+	for _, v := range encoder.BytesTokenizer(s) {
+		mw.Push(v)
+	}
+
+	// Push the min wise to the table
+	count_, err := t.Push(mw.GetSignature())
+	if err!= nil {
+		fmt.Println("Mousp !")
+	}
+
+	count := count_.(uint8)
+	return count, mw
+}
+
+func main() {
+
+	// Geneate two hash functions to be used by tge min hash algo
+	h1 := BuildHash64Function(42)
+	h2 := BuildHash64Function(43)
+
+	// Generate a count table to holds all the minhashs
+	table, err := encoder.NewCountMinSketchTable(100, encoder.UINT8)
+	if err != nil {
+		fmt.Println("Moups")
+	}
+
+	// Append the two strings...
+	stringtest := "un deux trois quatre cinq six sept huit neuf dix onze douze treize quatorze quinze seize dixsept dixhuit dixneuf vingt"
+	stringtest2 := "un deux troisq quatre cinqq six septq huit neufq dix onzeq douze treizeq quatorze quinzeq seize dixseptq dixhuit dixneufq vingt"
+
+	count, mw := add(table, stringtest, h1, h2)
+	fmt.Println(count)
+
+	count2, mw2 := add(table, stringtest2, h1, h2)
+	fmt.Println(count2)
+
+	count3, _ := add(table, stringtest, h1, h2)
+	fmt.Println(count3)
+
+	fmt.Println(mw.Similarity(mw2))
+
+	
 }
